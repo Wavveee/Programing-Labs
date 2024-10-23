ec661f663cac370976f7ca4c11d5f582ca4f94ed
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2024-10-23 18:59:23 +0300

    completed laboratory work No 4.3

---
+++ lab4_p3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    float z,y,x,x_step,y_step,x_start,y_start;
+
+    cout << "enter value to 'x'";
+        cin >> x;
+    cout << "enter value to 'y'";
+        cin >> y;
+
+    cout << "enter value of 'x_step'";
+        cin >> x_step;
+        // 0.3
+
+    cout << "enter value of 'y_step'";
+        cin >> y_step;
+        // 0.2
+ 
+    x_start= x;
+    y_start= y;
+
+    while(y>=0.5 && y<=1.5){
+        x= x_start;
+        while(x>=1 && x<=4){
+            z=log(cos(x*y-1)+2);
+
+            cout << setw(5) << fixed << setprecision(2) << x
+                 << setw(10) << fixed << setprecision(2) << y
+                 << setw(10) << fixed << setprecision(2) << z << endl;
+            x+=x_step;
+        };
+        y+=y_step;
+    };
+
+   return 0;
+}
