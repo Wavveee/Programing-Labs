570e51286d37a090d06b5ac7789ee06a89633253
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2024-11-05 19:21:27 +0200

    Update lab4_p2.cpp

--- C++/lab4_p2.cpp
+++ C++/lab4_p2.cpp
@@ -9,27 +9,25 @@ int main() {
    
     cout << "enter value to 'x'";
         cin >> x;
-     cout << "enter value of 'step'";
+    cout << "enter value of 'step'";
         cin >> step;
-    // крок 0.5
-       
-    if(x>=-2 && x<=0){
-       
-        while(x<=0){
-            y=sin(x)+exp(x);
-           
-            cout << setw(5) << fixed << setprecision(2) << x
-                 << setw(10) << fixed << setprecision(2) << y << endl;  
+
+    while (x>=-2 && x<=3) {
+
+        if (x>=-2 && x<=0) {
+
+             y=sin(x)+exp(x);
+
+        } else {
+
+            y=(M_PI - atan(x)) * (x - 0.3);
+
+        };
+
+        cout << setw(5) << fixed << setprecision(2) << x
+             << setw(10) << fixed << setprecision(2) << y << endl;
             x += step;
-        };   
-    }else if (x>0 && x<=3){
-        while(x<=3){
-             y=(M_PI - atan(x)) * (x - 0.3);
-             
-            cout << setw(5) << fixed << setprecision(2) << x
-                 << setw(10) << fixed << setprecision(2) << y << endl;
-             x += step;
-        }; 
+
     };
    
     return 0;
