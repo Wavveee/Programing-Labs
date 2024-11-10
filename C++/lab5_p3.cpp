549730e6dd2097e57474a49a4644e79cc61b4cf3
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2024-11-10 14:58:54 +0200

    Update lab5_p3.cpp

--- C++/lab5_p3.cpp
+++ C++/lab5_p3.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <math.h>
+#include <iomanip>
 using namespace std;
 
-int main() { 
+int main() {
 
-    float y=0,x_1,delta_x,x_k,x_k_prev;
+    float y=0,x_1,delta_x,x_k,x_k_prev; 
     int start,end;
 
     cout << "Enter value for 'start' ";
@@ -23,8 +24,17 @@ int main() {
       cin >> delta_x;
       //0.05
 
+    for(int i=1; i<start; i++){
+       
+    x_1+= delta_x;
+    cout << "test x_1  " << x_1 <<endl;
+    };
+   
+   
     x_k= x_1;
-    x_k_prev = x_k; 
+    x_k_prev = x_1 - delta_x;
+    cout << "test x_k_prev  " << x_k_prev<<endl;
+   
     for (int k = start; k <= end; ++k) {
 
         float num = pow(x_k + x_k_prev, 2);
@@ -32,11 +42,14 @@ int main() {
 
         y += num / denom;
 
-        x_k_prev = x_k; 
-        x_k += delta_x; 
+        x_k_prev = x_k;
+        x_k += delta_x;
+       
+        cout << "test y= " << y << endl;
     }
 
     cout << "Final sum is y= " << y <<endl;
 
     return 0;
 };
+
