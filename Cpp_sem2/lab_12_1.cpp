c8a90a6fbfc0679e1a505692f3f66f339d35015d
Anton Hvrylchyk <anton.g.2.9x@gmail.com>
2025-04-10 18:20:43 +0300

    --

---
+++ Cpp_sem2/lab_12_1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+const int SIZE = 20;
+string init_arr[SIZE] = { "Toyota", "Honda", "Ford", "Chevrolet", "BMW",
+    "Mercedes", "Audi", "Volkswagen", "Hyundai", "Kia",
+    "Nissan", "Mazda", "Subaru", "Lexus", "Jeep",
+    "Volvo", "Porsche", "Ferrari", "Lamborghini", "Peugeot" };
+void show_arr(string arr[], int numb);
+
+int main() {
+    cout << "We have" << endl;
+    show_arr(init_arr, SIZE);
+
+    while (true) {
+
+        cout << "how many we show this time?" << endl;
+        int numb{ 0 };
+        cin >> numb;
+
+        if (numb > 0 && numb < SIZE) {
+            show_arr(init_arr, numb);
+        }
+        else {
+            cout << "goodbye" << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void show_arr(string arr[], int numb) {
+    for (int i = 0; i < numb; ++i) {
+        cout << i + 1 << ". " << arr[i] << std::endl;
+    }
+};
